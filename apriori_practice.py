4c0e91344454db3eb35900664b338c751f55d1c1
Nathan <50347415+Nato33@users.noreply.github.com>
2019-11-15 14:12:10 -0500

    Add files via upload

---
+++ apriori_practice.py
@@ -0,0 +1,27 @@
+#Apriori
+
+#Importing the Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#importing the dataset
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+transactions = []
+for i in range (0, 7501):
+    transactions.append([str(dataset.values[i,j]) for j in range(0,20)])
+    
+# Training Apriori on the dataset
+from apyori import apriori
+rules = apriori(transactions, min_support = .003, min_confidence = 0.2, min_lift = 3, min_length = 2)
+
+# Visualizing the rules
+results = list(rules)
+
+results_list = []
+
+
+
+for i in range(0, len(results)):
+
+    results_list.append('\RESULTS:\t' + str(results[i][2]))
