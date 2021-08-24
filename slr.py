0afc5097b056f57b09c42e8522b574d81f613bea
Nathan <50347415+Nato33@users.noreply.github.com>
2021-08-24 16:07:26 -0400

    Update slr.py

--- slr.py
+++ slr.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #importing the dataset
-dataset = pd.read_csv('Salary_Data.csv')
+dataset = pd.read_csv('.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 1].values
 
