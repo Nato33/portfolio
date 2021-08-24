35f2d223f460f4deee5450875cdbdebf73f6d608
Nathan <50347415+Nato33@users.noreply.github.com>
2021-08-24 16:04:31 -0400

    Update kmeans_clustering_practice.py

--- kmeans_clustering_practice.py
+++ kmeans_clustering_practice.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #importing the dataset
-dataset = pd.read_csv('Mall_Customers.csv')
+dataset = pd.read_csv('')
 X = dataset.iloc[:, [3,4]].values
 
 #Using the 'Elbow Method' to find the Optimal # of clusters
